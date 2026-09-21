4d7e8a1968c28a29bb9a035c810709adc8256216
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-21 11:56:06 +0400

    feat(task-383): implement recursive palindrome checker recursion

--- src/RecursivePalindromeChecker.java
+++ src/RecursivePalindromeChecker.java
@@ -10,6 +10,8 @@ public class RecursivePalindromeChecker {
     }
 
     public static boolean isPalindrome(String word) {
-        return false;
+        if (word.length() <= 1) return true;
+        if (word.charAt(0) != word.charAt(word.length() - 1)) return false;
+        return isPalindrome(word.substring(1, word.length() - 1));
     }
 }
