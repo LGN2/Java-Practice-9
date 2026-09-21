60a6efb387369f1220b8e76dc2640335bc72c5f1
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-21 11:55:38 +0400

    feat(task-376): add print numbers up structure

---
+++ src/PrintNumbersUp.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PrintNumbersUp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive number: ");
+        if (!scanner.hasNextInt()) System.out.println("Invalid input. Please enter an integer.");
+        else {
+            int number = scanner.nextInt();
+            if (number <= 0) System.out.println("Please enter a number greater than zero.");
+            else printUp(number);
+        }
+        scanner.close();
+    }
+
+    public static void printUp(int number) {
+        System.out.println("Recursive number printing will be added next.");
+    }
+}
